145a3427dc1b5dd928f79bd72b2533e4ce43cce5
Triiist4n <hutter230164@sr.htlweiz.at>
2025-12-01 08:40:37 +0100

    01.12.2025

--- Waisenkinder/Data/obj/Debug/net8.0/Data.AssemblyInfo.cs
+++ Waisenkinder/Data/obj/Debug/net8.0/Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dc1090951f77a246758676293d8e4ada176b170a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5cffe8d26208acfdcedd0744c9149a3f2c9b0bc3")]
 [assembly: System.Reflection.AssemblyProductAttribute("Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
