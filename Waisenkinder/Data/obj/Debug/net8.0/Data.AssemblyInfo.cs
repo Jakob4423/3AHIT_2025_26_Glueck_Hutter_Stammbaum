dc1090951f77a246758676293d8e4ada176b170a
Jakob4423 <jakob.glueck01@gmail.com>
2025-12-01 08:06:19 +0100

    1.12.2025

--- Waisenkinder/Data/obj/Debug/net8.0/Data.AssemblyInfo.cs
+++ Waisenkinder/Data/obj/Debug/net8.0/Data.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Data")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4675ed4bc4cd79b35af2334ab7f9490a125c9b95")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9f2fd960875468cd45215c10382b75749d7ca372")]
 [assembly: System.Reflection.AssemblyProductAttribute("Data")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Data")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
